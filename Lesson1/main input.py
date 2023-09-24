d5a42cdcbd6783a3665ea8d8426e9a9c317345af
KostyaSnigur <kostyasnigur@gmail.com>
2023-09-24 12:08:32 +0300

    5

--- Lesson1/main input.py
+++ Lesson1/main input.py
@@ -1,8 +1,15 @@
 name = input("What's your name>>> ")
 age = int(input("How old are you >>> "))
+
+
+# formatted stringst
+# snake case exp. datacase_results
+ 
+upper_name = name.upper()
+
 print(type(name))
 print(type(age))
-print(f"Hello Mr: {name} {age}")
+print(f"Hello Mr: {name} $ {upper_name} {age}")
 
 print("*" * 10)
 print("Mr: " + name + str(age))
