401c67dcdce21de3e9e07250eee590209fb8f34e
KostyaSnigur <kostyasnigur@gmail.com>
2023-09-24 10:40:50 +0300

    3

--- Lesson1/main input.py
+++ Lesson1/main input.py
@@ -1,2 +1,8 @@
-name = input(">>> ")
-print("Mr: " + name)
+name = input("What's your name>>> ")
+age = int(input("How old are you >>> "))
+print(type(name))
+print(type(age))
+print(f"Hello Mr: {name} {age}")
+
+print("*" * 10)
+print("Mr: " + name + str(age))
