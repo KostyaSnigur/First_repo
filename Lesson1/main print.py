9acebe4ea49697a7538b9670c004eed4062049bb
KostyaSnigur <kostyasnigur@gmail.com>
2023-09-24 10:28:27 +0300

    second

---
+++ Lesson1/main print.py
@@ -0,0 +1,15 @@
+str1 = "Hello"
+str2 = 'Hello'
+
+str3 = """Hello""" # doc string документація
+str4 = """Kostya
+Hello
+Hello
+Hello
+"""
+
+print("Hello World!")
+print(str1)
+print(str2)
+print(str3)
+print(str4)
--- Lesson1/test.py
+++ Lesson1/test.py
@@ -5,8 +5,9 @@ int = 12323
 float = 3.14
 
 list, tuple, range
-list[1,2,3,5,66]
+[1,2,3,5,66]
+list()
 
 dict
-bool = True False
+bool = True, False
 bytearray
